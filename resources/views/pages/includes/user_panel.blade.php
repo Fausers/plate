78814e61d945d35d7b78292e4264d265ec93cdce
Fausers <felixthobias@gmail.com>
2021-10-21 11:56:14 +0300

    2

---
+++ resources/views/pages/includes/user_panel.blade.php
@@ -0,0 +1,116 @@
+<div id="kt_quick_user" class="offcanvas offcanvas-right p-10">
+			<!--begin::Header-->
+			<div class="offcanvas-header d-flex align-items-center justify-content-between pb-5">
+				<h3 class="font-weight-bold m-0">User Profile
+				<small class="text-muted font-size-sm ml-2">12 messages</small></h3>
+				<a href="#" class="btn btn-xs btn-icon btn-light btn-hover-primary" id="kt_quick_user_close">
+					<i class="ki ki-close icon-xs text-muted"></i>
+				</a>
+			</div>
+			<!--end::Header-->
+			<!--begin::Content-->
+			<div class="offcanvas-content pr-5 mr-n5">
+				<!--begin::Header-->
+				<div class="d-flex align-items-center mt-5">
+					<div class="symbol symbol-100 mr-5">
+						<div class="symbol-label"
+                        style="background-image:url('@if (Auth::user()->profile->avatar_status == 1)
+                                                {{ Auth::user()->profile->avatar }} @else
+                                                {{ Gravatar::get(Auth::user()->email) }} @endif'
+                            )"
+                ></div>
+						<i class="symbol-badge bg-success"></i>
+					</div>
+					<div class="d-flex flex-column">
+						<a href="#" class="font-weight-bold font-size-h5 text-dark-75 text-hover-primary">
+                            {{Auth::user()->first_name}} {{Auth::user()->last_name}}
+                        </a>
+						<div class="text-muted mt-1">
+                            @foreach (Auth::user()->roles()->pluck('name') as $role)
+                                {{ $role }} @if(!$loop->last) , @endif
+                            @endforeach
+                        </div>
+						<div class="navi mt-2">
+							<a href="#" class="navi-item">
+								<span class="navi-link p-0 pb-2">
+									<span class="navi-icon mr-1">
+										<span class="svg-icon svg-icon-lg svg-icon-primary">
+											<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Communication/Mail-notification.svg-->
+											<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+												<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+													<rect x="0" y="0" width="24" height="24" />
+													<path d="M21,12.0829584 C20.6747915,12.0283988 20.3407122,12 20,12 C16.6862915,12 14,14.6862915 14,18 C14,18.3407122 14.0283988,18.6747915 14.0829584,19 L5,19 C3.8954305,19 3,18.1045695 3,17 L3,8 C3,6.8954305 3.8954305,6 5,6 L19,6 C20.1045695,6 21,6.8954305 21,8 L21,12.0829584 Z M18.1444251,7.83964668 L12,11.1481833 L5.85557487,7.83964668 C5.4908718,7.6432681 5.03602525,7.77972206 4.83964668,8.14442513 C4.6432681,8.5091282 4.77972206,8.96397475 5.14442513,9.16035332 L11.6444251,12.6603533 C11.8664074,12.7798822 12.1335926,12.7798822 12.3555749,12.6603533 L18.8555749,9.16035332 C19.2202779,8.96397475 19.3567319,8.5091282 19.1603533,8.14442513 C18.9639747,7.77972206 18.5091282,7.6432681 18.1444251,7.83964668 Z" fill="#000000" />
+													<circle fill="#000000" opacity="0.3" cx="19.5" cy="17.5" r="2.5" />
+												</g>
+											</svg>
+											<!--end::Svg Icon-->
+										</span>
+									</span>
+									<span class="navi-text text-muted text-hover-primary">{{Auth::user()->email}}</span>
+								</span>
+							</a>
+							<a class="btn btn-sm btn-light-primary font-weight-bolder py-2 px-5" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+						</div>
+					</div>
+				</div>
+				<!--end::Header-->
+				<!--begin::Separator-->
+				<div class="separator separator-dashed mt-8 mb-5"></div>
+				<!--end::Separator-->
+
+				<!--begin::Separator-->
+				<div class="separator separator-dashed my-7"></div>
+				<!--end::Separator-->
+
+                @if(Auth::user()->hasRole('manager'))
+				<!--begin::Shops-->
+				<div>
+					<!--begin:Heading-->
+					<h5 class="mb-5">My Shops</h5>
+					<!--end:Heading-->
+
+                    @foreach(Auth::user()->shops as $shop)
+					<!--begin::Item-->
+					<div class="d-flex align-items-center @if($shop->id == Auth::user()->active_shop) bg-light-success @else bg-light-warning @endif rounded p-5 gutter-b">
+						<span class="svg-icon svg-icon-warning mr-5">
+							<span class="svg-icon svg-icon-lg">
+								<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Home/Library.svg-->
+								<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+									<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+										<rect x="0" y="0" width="24" height="24" />
+										<path d="M5,3 L6,3 C6.55228475,3 7,3.44771525 7,4 L7,20 C7,20.5522847 6.55228475,21 6,21 L5,21 C4.44771525,21 4,20.5522847 4,20 L4,4 C4,3.44771525 4.44771525,3 5,3 Z M10,3 L11,3 C11.5522847,3 12,3.44771525 12,4 L12,20 C12,20.5522847 11.5522847,21 11,21 L10,21 C9.44771525,21 9,20.5522847 9,20 L9,4 C9,3.44771525 9.44771525,3 10,3 Z" fill="#000000" />
+										<rect fill="#000000" opacity="0.3" transform="translate(17.825568, 11.945519) rotate(-19.000000) translate(-17.825568, -11.945519)" x="16.3255682" y="2.94551858" width="3" height="18" rx="1" />
+									</g>
+								</svg>
+								<!--end::Svg Icon-->
+							</span>
+						</span>
+						<div class="d-flex flex-column flex-grow-1 mr-2">
+							<a href="#" class="font-weight-normal text-dark-75 text-hover-primary font-size-lg mb-1">{{$shop->name}}</a>
+							<span class="text-muted font-size-sm">Active Products {{count($shop->products)}}</span>
+						</div>
+						<span class="font-weight-bolder @if($shop->id == Auth::user()->active_shop) text-success @else text-danger @endif py-1 font-size-lg">
+                            @if($shop->id == Auth::user()->active_shop) Selected
+                            @else
+                                <a onclick="f('{{$shop->name}} Is Activated')" href="{{route('activate_shop',[$shop->id])}}">Activate</a>
+                            @endif
+                        </span>
+					</div>
+					<!--end::Item-->
+                    @endforeach
+
+
+					<!--end::Item-->
+                </div>
+				<!--end::Shops-->
+                    @endif
+			</div>
+			<!--end::Content-->
+		</div>
